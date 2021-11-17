e023fa81b9ecbf5b57705cf76a50d4e0dc6c44ef
pradt2 <pradt2@localhost>
2021-11-17 16:35:30 +0000

    Strip log messages

--- run.sh
+++ run.sh
@@ -2,12 +2,8 @@
 cd "$(dirname "$0")"
 timestamp=$(date --utc)
 git checkout master && \
-echo "Checked out master" && \
 git pull && \
-echo "Pulled from master" && \
 cargo run && \
-echo "Ran the task using Cargo" && \
 git add . && \
 git commit -m "Check ${timestamp}" && \
-git push && \
-echo "Pushed the latest version"
+git push
