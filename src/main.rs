d5396b0c450ca96bb218af07b95f9338a82c30c5
pradt2 <pradt2@localhost>
2022-04-07 01:00:39 +0100

    WIP towards Geo information

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::time::Duration;
 use tokio::time::Instant;
 use crate::utils::join_all_with_semaphore;
 use crate::outputs::{ValidHosts, ValidIpV4s, ValidIpV6s};
@@ -12,45 +13,80 @@ mod servers;
 mod stun;
 mod utils;
 mod outputs;
+mod geoip;
+
+const CONCURRENT_SOCKETS_USED_LIMIT: usize = 64;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> io::Result<()> {
-    pretty_env_logger::init_timed();
+    pretty_env_logger::init();
 
-    let stun_servers = servers::get_stun_servers().await?;
-    info!("Loaded {} stun server candidates", stun_servers.len());
+    let mut client = geoip::CachedIpGeolocationIpClient::default().await?;
+    let result = client
+        .get_hostname_geoip_info("86.10.243.212")
+        .await;
+
+    client.save().await?;
+
+    let stun_servers = servers::get_stun_servers()
+        .await?;
+
+    let stun_servers_count = stun_servers.len();
+    info!("Loaded {} stun server hosts", stun_servers.len());
 
     let stun_server_test_results = stun_servers.into_iter()
         .map(|candidate| {
             async move {
                 let test_result = stun::test_udp_stun_server(candidate).await;
+                print_stun_server_status(&test_result);
                 test_result
             }
         })
         .collect::<Vec<_>>();
 
     let timestamp = Instant::now();
-    let stun_server_test_results = join_all_with_semaphore(stun_server_test_results.into_iter(), 100).await;
-
-    write_stun_server_summary(&stun_server_test_results);
+    let stun_server_test_results = join_all_with_semaphore(stun_server_test_results.into_iter(), CONCURRENT_SOCKETS_USED_LIMIT).await;
 
     ValidHosts::default(&stun_server_test_results).save().await?;
     ValidIpV4s::default(&stun_server_test_results).save().await?;
     ValidIpV6s::default(&stun_server_test_results).save().await?;
 
-    info!("Finished in {:?}", timestamp.elapsed());
+    write_stun_server_summary(stun_servers_count, &stun_server_test_results,timestamp.elapsed());
+
     Ok(())
 }
 
-fn write_stun_server_summary(results: &Vec<StunServerTestResult>) {
-    let mut all_ok = 0;
+fn print_stun_server_status(test_result: &StunServerTestResult) {
+    if test_result.is_healthy() {
+        info!("{:<25} -> Host is healthy", test_result.server.hostname);
+    } else if !test_result.is_resolvable() {
+        info!("{:<25} -> Host is not resolvable", test_result.server.hostname);
+    } else if test_result.is_partial_timeout() {
+        info!("{:<25} -> Host times out on some sockets", test_result.server.hostname);
+    } else if test_result.is_timeout() {
+        info!("{:<25} -> Host times out on all sockets", test_result.server.hostname);
+    } else {
+        info!("{:<25} -> Host behaves in an unexpected way. Run with RUST_LOG=DEBUG for more info", test_result.server.hostname);
+        for socket_test in &test_result.socket_tests {
+            match &socket_test.result {
+                StunSocketResponse::HealthyResponse { .. } => { debug!("{:<25} -> Socket {:<21} returned a healthy response", test_result.server.hostname, socket_test.socket) }
+                StunSocketResponse::InvalidMappingResponse { expected, actual, rtt } => { debug!("{:<25} -> Socket {:<21} returned an invalid mapping: expected={} actual={}", test_result.server.hostname, socket_test.socket, expected, actual) }
+                StunSocketResponse::Timeout { deadline } => { debug!("{:<25} -> Socket {:<21} timed out after {:?}", test_result.server.hostname, socket_test.socket, deadline) }
+                StunSocketResponse::UnexpectedError { err } => { debug!("{:<25} -> Socket {:<21} returned an unexpected error: {}", test_result.server.hostname, socket_test.socket, err) }
+            }
+        }
+    }
+}
+
+fn write_stun_server_summary(candidate_hosts_count: usize, results: &Vec<StunServerTestResult>, time_taken: Duration) {
+    let mut healthy = 0;
     let mut dns_unresolved = 0;
     let mut partial_timeout = 0;
     let mut timeout = 0;
-    let mut other = 0;
+    let mut unexpected_err = 0;
     results.iter().for_each(|server_test_result| {
         if server_test_result.is_healthy() {
-            all_ok += 1;
+            healthy += 1;
         } else if !server_test_result.is_resolvable() {
             dns_unresolved += 1;
         } else if server_test_result.is_partial_timeout() {
@@ -58,19 +94,15 @@ fn write_stun_server_summary(results: &Vec<StunServerTestResult>) {
         } else if server_test_result.is_timeout() {
             timeout += 1;
         } else {
-            other += 1;
-            for socket_test in &server_test_result.socket_tests {
-                match &socket_test.result {
-                    StunSocketResponse::HealthyResponse { .. } => { info!("Unhealthy host {:<25} -> socket {:<21} returned a healthy response", server_test_result.server.hostname, socket_test.socket) }
-                    StunSocketResponse::InvalidMappingResponse { expected, actual, rtt } => { info!("Unhealthy host {:<25} -> socket {:<21} returned an invalid mapping: expected={} actual={}", server_test_result.server.hostname, socket_test.socket, expected, actual) }
-                    StunSocketResponse::Timeout { deadline } => { info!("Unhealthy host {:<25} -> socket {:<21} timed out after {:?}", server_test_result.server.hostname, socket_test.socket, deadline) }
-                    StunSocketResponse::UnexpectedError { err } => { info!("Unhealthy host {:<25} -> socket {:<21} returned an unexpected error: {}", server_test_result.server.hostname, socket_test.socket, err) }
-                }
-            }
+            unexpected_err += 1;
         }
     });
     info!(
-        "Stats -> OK={}, DNS failure={}, p/Timeout={}, Timeout={}, Other={}",
-        all_ok, dns_unresolved, partial_timeout, timeout, other
+        "Statistics -> Tested={}, Healthy={}, DNS failure={}, partial Timeout={}, Timeout={}, Unexpected err={}. Finished in {:?}",
+        candidate_hosts_count, healthy, dns_unresolved, partial_timeout, timeout, unexpected_err, time_taken
     );
+
+    if healthy == 0 {
+        warn!("No healthy hosts found! Are you behind NAT?")
+    }
 }
--- src/servers.rs
+++ src/servers.rs
@@ -1,4 +1,3 @@
-use std::collections::HashSet;
 use tokio::io;
 
 const FILE_PATH: &str = "candidates.txt";
@@ -19,9 +18,9 @@ pub async fn get_stun_servers() -> io::Result<Vec<StunServer>> {
     let stun_servers = tokio::fs::read_to_string(FILE_PATH).await?
         .split('\n')
         .map(|s| s.trim())
+        .filter(|s| !s.is_empty())
         .filter(|s| !s.starts_with('#'))
-        .collect::<HashSet<_>>()
-        .into_iter()
+        .filter(|s| !s.starts_with("//"))
         .map(|stun_server_str| {
             let (hostname, port) = stun_server_str.split_once(':').unwrap();
             StunServer {
