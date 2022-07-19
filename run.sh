cddf6dbc89035265f6fcf2fbd54eea7de345da26
pradt2 <12902844+pradt2@users.noreply.github.com>
2022-07-19 18:55:11 +0100

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-cd "$(dirname "$0")"
-timestamp=$(date --utc)
-git checkout master && \
-git pull && \
-RUST_LOG=info cargo run && \
-git add . && \
-git commit -m "Check ${timestamp}" && \
-git push
