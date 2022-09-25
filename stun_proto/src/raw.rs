a97106fe5a4ac4fe9763688032dad904e42db5ac
pradt2 <pradt2@localhost>
2022-09-25 22:06:20 +0100

    Stun proto attempt 3

--- stun_proto/src/raw.rs
+++ stun_proto/src/raw.rs
@@ -1,85 +1,58 @@
 pub struct AssertedRawReader<'a> {
-    message_type: &'a [u8; 2],
-    message_length: &'a [u8; 2],
-    transaction_id: &'a [u8; 16],
-    attribute_bytes: &'a [u8],
-    attr_iter: AssertedRawAttributesReaderIterator<'a>
+    pub typ: &'a [u8; 2],
+    pub len: &'a [u8; 2],
+    pub tid: &'a [u8; 16],
+    pub attrs: &'a [u8],
+    attr_iter: AssertedRawAttributesReaderIterator<'a>,
 }
 
 impl<'a> AssertedRawReader<'a> {
     pub fn new(bytes: &'a [u8]) -> Option<Self> {
-        let message_type = bytes.get(0..2)?.try_into().ok()?;
-        let message_length = bytes.get(2..4)?.try_into().ok()?;
-        let transaction_id = bytes.get(4..20)?.try_into().ok()?;
-        let attribute_bytes = bytes.get(20..)?;
-        let attr_iter = AssertedRawAttributesReaderIterator::new(attribute_bytes)?;
+        let reader = RawReader::new(bytes);
+
+        let attr_iter = AssertedRawAttributesReaderIterator::new(reader.attrs?)?;
 
         Some(Self {
-            message_type,
-            message_length,
-            transaction_id,
-            attribute_bytes,
+            typ: reader.typ?,
+            len: reader.len?,
+            tid: reader.tid?,
+            attrs: reader.attrs?,
             attr_iter,
         })
     }
 
-    pub fn get_message_type(&self) -> &[u8; 2] {
-        self.message_type
-    }
-
-    pub fn get_message_length(&self) -> &[u8; 2] {
-        self.message_length
-    }
-
-    pub fn get_transaction_id(&self) -> &[u8; 16] {
-        self.transaction_id
-    }
-
-    pub fn get_attributes(&self) -> AssertedRawAttributesReaderIterator {
-        self.attr_iter.copy()
+    pub fn attrs(&self) -> AssertedRawAttributesReaderIterator {
+        self.attr_iter.clone()
     }
 }
 
 pub struct AssertedRawAttributeReader<'a> {
-    attr_type: &'a [u8; 2],
-    attr_length: &'a [u8; 2],
-    attr_value_with_padding: &'a [u8],
+    pub typ: &'a [u8; 2],
+    pub len: &'a [u8; 2],
+    pub val: &'a [u8],
 }
 
 impl<'a> AssertedRawAttributeReader<'a> {
-    fn new(bytes: &'a [u8]) -> Option<Self> {
-        let attr_type = bytes.get(0..2)?.try_into().ok()?;
-        let attr_length: &[u8; 2] = bytes.get(2..4)?.try_into().ok()?;
-        let attr_value_with_padding = bytes.get(4..)?;
+    fn new(reader: &RawAttributeReader<'a>) -> Option<Self> {
+        let attr_value_with_padding = reader.val?;
 
         if attr_value_with_padding.len() != get_nearest_greater_multiple_of_4(attr_value_with_padding.len()) {
             return None;
         }
 
-        if u16::from_be_bytes(*attr_length) as usize > attr_value_with_padding.len() {
+        if u16::from_be_bytes(*reader.len?) as usize > attr_value_with_padding.len() {
             return None;
         }
 
         Some(Self {
-            attr_type,
-            attr_length,
-            attr_value_with_padding,
+            typ: reader.typ?,
+            len: reader.len?,
+            val: attr_value_with_padding,
         })
-    }
-
-    pub fn get_attribute_type(&self) -> &[u8; 2] {
-        self.attr_type
-    }
-
-    pub fn get_attribute_length(&self) -> &[u8; 2] {
-        self.attr_length
-    }
-
-    pub fn get_attribute_value_with_padding(&self) -> &[u8] {
-        self.attr_value_with_padding
     }
 }
 
+#[derive(Clone)]
 pub struct AssertedRawAttributesReaderIterator<'a> {
     raw_iter: RawAttributesReaderIterator<'a>,
 }
@@ -94,12 +67,6 @@ impl<'a> AssertedRawAttributesReaderIterator<'a> {
             raw_iter: RawAttributesReaderIterator::new(bytes)
         })
     }
-
-    fn copy(&self) -> Self {
-        Self {
-            raw_iter: self.raw_iter.copy()
-        }
-    }
 }
 
 impl<'a> Iterator for AssertedRawAttributesReaderIterator<'a> {
@@ -111,59 +78,50 @@ impl<'a> Iterator for AssertedRawAttributesReaderIterator<'a> {
 }
 
 pub struct RawReader<'a> {
-    bytes: &'a [u8],
+    pub typ: Option<&'a [u8; 2]>,
+    pub len: Option<&'a [u8; 2]>,
+    pub tid: Option<&'a [u8; 16]>,
+    pub attrs: Option<&'a [u8]>,
+    attr_iter: RawAttributesReaderIterator<'a>,
 }
 
 impl<'a> RawReader<'a> {
     pub fn new(bytes: &'a [u8]) -> Self {
-        Self { bytes }
+        Self {
+            typ: bytes.get(0..2).map(|s| s.try_into().ok()).flatten(),
+            len: bytes.get(2..4).map(|s| s.try_into().ok()).flatten(),
+            tid: bytes.get(4..20).map(|s| s.try_into().ok()).flatten(),
+            attrs: bytes.get(20..),
+            attr_iter: RawAttributesReaderIterator::new(bytes.get(20..).unwrap_or(&[])),
+        }
     }
 
-    pub fn get_message_type(&self) -> Option<&[u8; 2]> {
-        self.bytes.get(0..2)?.try_into().ok()
-    }
-
-    pub fn get_message_length(&self) -> Option<&[u8; 2]> {
-        self.bytes.get(2..4)?.try_into().ok()
-    }
-
-    pub fn get_transaction_id(&self) -> Option<&[u8; 16]> {
-        self.bytes.get(4..20)?.try_into().ok()
-    }
-
-    pub fn get_attributes(&self) -> RawAttributesReaderIterator<'a> {
-        RawAttributesReaderIterator::new(self.bytes.get(20..).unwrap_or(&[]))
+    pub fn attrs(&self) -> RawAttributesReaderIterator {
+        self.attr_iter.clone()
     }
 }
 
 pub struct RawAttributeReader<'a> {
-    bytes: &'a [u8],
+    pub typ: Option<&'a [u8; 2]>,
+    pub len: Option<&'a [u8; 2]>,
+    pub val: Option<&'a [u8]>,
 }
 
 impl<'a> RawAttributeReader<'a> {
     fn new(bytes: &'a [u8]) -> Self {
         Self {
-            bytes
+            typ: bytes.get(0..2).map(|s| s.try_into().ok()).flatten(),
+            len: bytes.get(2..4).map(|s| s.try_into().ok()).flatten(),
+            val: bytes.get(4..),
         }
     }
 
-    pub fn get_attribute_type(&self) -> Option<&[u8; 2]> {
-        self.bytes.get(0..2)?.try_into().ok()
-    }
-
-    pub fn get_attribute_length(&self) -> Option<&[u8; 2]> {
-        self.bytes.get(2..4)?.try_into().ok()
-    }
-
-    pub fn get_attribute_value_with_padding(&self) -> Option<&[u8]> {
-        self.bytes.get(4..)
-    }
-
     pub fn asserted(&self) -> Option<AssertedRawAttributeReader<'a>> {
-        AssertedRawAttributeReader::new(self.bytes)
+        AssertedRawAttributeReader::new(self)
     }
 }
 
+#[derive(Clone)]
 pub struct RawAttributesReaderIterator<'a> {
     bytes: &'a [u8],
 }
@@ -172,12 +130,6 @@ impl<'a> RawAttributesReaderIterator<'a> {
     fn new(bytes: &'a [u8]) -> Self {
         Self {
             bytes
-        }
-    }
-
-    fn copy(&self) -> Self {
-        Self {
-            bytes: self.bytes
         }
     }
 
@@ -219,110 +171,76 @@ impl<'a> Iterator for RawAttributesReaderIterator<'a> {
     }
 }
 
+pub struct AssertedRawWriter<'a> {
+    pub typ: &'a mut [u8; 2],
+    pub len: &'a mut [u8; 2],
+    pub tid: &'a mut [u8; 16],
+    pub attrs: &'a mut [u8],
+}
+
+impl<'a> AssertedRawWriter<'a> {
+    pub fn new(bytes: &'a mut [u8]) -> Option<Self> {
+        let reader = RawWriter::new(bytes);
+
+        Some(Self {
+            typ: reader.typ?,
+            len: reader.len?,
+            tid: reader.tid?,
+            attrs: reader.attrs?,
+        })
+    }
+}
+
 pub struct RawWriter<'a> {
-    bytes: &'a mut [u8],
+    pub typ: Option<&'a mut [u8; 2]>,
+    pub len: Option<&'a mut [u8; 2]>,
+    pub tid: Option<&'a mut [u8; 16]>,
+    pub attrs: Option<&'a mut [u8]>,
     cursor: usize,
 }
 
 impl<'a> RawWriter<'a> {
     pub fn new(bytes: &'a mut [u8]) -> Self {
-        Self { bytes, cursor: 0 }
-    }
+        let (typ, bytes) = split_at_mut(bytes);
+        let (len, bytes) = split_at_mut(bytes);
+        let (tid, bytes) = split_at_mut(bytes);
+        let attrs = if tid.is_none() { None } else { Some(bytes) };
 
-    pub fn get_message_type(&mut self) -> Option<&mut [u8; 2]> {
-        self.bytes.get_mut(0..2)?.try_into().ok()
-    }
-
-    pub fn get_message_length(&mut self) -> Option<&mut [u8; 2]> {
-        self.bytes.get_mut(2..4)?.try_into().ok()
-    }
-
-    pub fn get_transaction_id(&mut self) -> Option<&mut [u8; 16]> {
-        self.bytes.get_mut(4..20)?.try_into().ok()
-    }
-
-
-
-    pub fn get_attributes(&self) -> RawAttributesWriterIterator<'a> {
-        RawAttributesWriterIterator::new(self.bytes.get_mut(20..).unwrap_or(&mut []))
-    }
-}
-
-pub struct RawAttributeWriter<'a> {
-    bytes: &'a mut [u8],
-}
-
-impl<'a> RawAttributeWriter<'a> {
-    fn new(bytes: &'a mut [u8]) -> Self {
         Self {
-            bytes
+            typ,
+            len,
+            tid,
+            attrs,
+            cursor: 0,
         }
     }
 
-    pub fn get_attribute_type(&mut self) -> Option<&mut [u8; 2]> {
-        self.bytes.get_mut(0..2)?.try_into().ok()
+    fn add_attr(&mut self, typ: &[u8; 2], len: &[u8; 2], val: &[u8]) -> Option<()> {
+        self.attrs?.get(self.cursor..self.cursor + 4 + val.len())?; // fail early if the entire attr cannot fit
+
+        self.attrs.as_mut()?.get_mut(self.cursor..self.cursor + 2)?.copy_from_slice(typ);
+        self.cursor += 2;
+
+        self.attrs.as_mut()?.get_mut(self.cursor..self.cursor + 2)?.copy_from_slice(len);
+        self.cursor += 2;
+
+        self.attrs.as_mut()?.get_mut(self.cursor..self.cursor + val.len())?.copy_from_slice(val);
+        self.cursor += val.len();
+
+        Some(())
     }
 
-    pub fn get_attribute_length(&mut self) -> Option<&[u8; 2]> {
-        self.bytes.get_mut(2..4)?.try_into().ok()
-    }
-
-    pub fn get_attribute_value_with_padding(&mut self) -> Option<&mut [u8]> {
-        self.bytes.get_mut(4..)
-    }
-
-    pub fn asserted(&mut self) -> Option<AssertedRawAttributeReader<'a>> {
-        AssertedRawAttributeReader::new(self.bytes)
-    }
-}
-
-pub struct RawAttributesWriterIterator<'a> {
-    bytes: &'a mut [u8],
-}
-
-impl<'a> RawAttributesWriterIterator<'a> {
-    fn new(bytes: &'a mut [u8]) -> Self {
-        Self {
-            bytes
-        }
-    }
-
-    fn asserted(&self) -> Option<AssertedRawAttributesReaderIterator> {
-        AssertedRawAttributesReaderIterator::new(self.bytes)
+    fn add_attr2<T: FnOnce(&[u8; 2], &[u8; 2], &[u8]) -> u16>(&mut self, f: T) {
+        let used = f(self)
     }
 }
 
-impl<'a> Iterator for RawAttributesWriterIterator<'a> {
-    type Item = RawAttributeWriter<'a>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.bytes.len() == 0 { return None; }
-
-        let declared_value_size = read_u16_be(self.bytes, 2);
-
-        match declared_value_size {
-            None => {
-                let reader = RawAttributeWriter::new(self.bytes);
-                self.bytes = &[];
-                Some(reader)
-            }
-            Some(declared_value_size) => match self.bytes.get_mut(0..4 + get_nearest_greater_multiple_of_4(declared_value_size as usize)) {
-                None => {
-                    let reader = RawAttributeWriter::new(self.bytes);
-                    self.bytes = &[];
-                    Some(reader)
-                }
-                Some(attr_bytes_incl_padding) => {
-                    let reader = RawAttributeWriter::new(attr_bytes_incl_padding);
-                    match self.bytes.get_mut(attr_bytes_incl_padding.len()..) {
-                        None => self.bytes = &[],
-                        Some(next_argument_bytes) => self.bytes = next_argument_bytes,
-                    };
-                    Some(reader)
-                }
-            }
-        }
+fn split_at_mut<const N: usize>(bytes: &mut [u8]) -> (Option<&mut [u8; N]>, &mut [u8]) {
+    if bytes.len() > N {
+        return (None, bytes);
     }
+    let (chunk, rest) = bytes.split_at_mut(N);
+    (chunk.try_into().ok(), rest)
 }
 
 fn read_u16_be(bytes: &[u8], offset: usize) -> Option<u16> {
@@ -334,4 +252,62 @@ fn read_u16_be(bytes: &[u8], offset: usize) -> Option<u16> {
 
 fn get_nearest_greater_multiple_of_4(len: usize) -> usize {
     (len + 3) & !3
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const MSG: [u8; 28] = [
+        0x00, 0x01,                     // type: Binding Request
+        0x00, 0x08,                     // length: 8 (header does not count)
+        0x21, 0x12, 0xA4, 0x42,         // magic cookie
+        0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x01,         // transaction id (16 bytes total incl. magic cookie)
+        0x00, 0x03,                     // type: ChangeRequest
+        0x00, 0x04,                     // value length
+        0x00, 0x00, 0x00, 0x40 | 0x20,  // change both ip and port
+    ];
+
+    #[test]
+    fn read_raw() {
+        let reader = RawReader::new(&MSG);
+        assert_eq!(&MSG[0..2], reader.typ.unwrap());
+        assert_eq!(&MSG[2..4], reader.len.unwrap());
+        assert_eq!(&MSG[4..20], reader.tid.unwrap());
+        assert_eq!(&MSG[20..], reader.attrs.unwrap());
+
+        assert_eq!(1, reader.attrs().count());
+        let attr_reader = reader.attrs().next().unwrap();
+
+        assert_eq!(&MSG[20..22], attr_reader.typ.unwrap());
+        assert_eq!(&MSG[22..24], attr_reader.len.unwrap());
+        assert_eq!(&MSG[24..28], attr_reader.val.unwrap());
+    }
+
+    #[test]
+    fn read_asserted() {
+        let reader = AssertedRawReader::new(&MSG).unwrap();
+        assert_eq!(&MSG[0..2], reader.typ);
+        assert_eq!(&MSG[2..4], reader.len);
+        assert_eq!(&MSG[4..20], reader.tid);
+        assert_eq!(&MSG[20..], reader.attrs);
+
+        assert_eq!(1, reader.attrs().count());
+        let attr_reader = reader.attrs().next().unwrap();
+
+        assert_eq!(&MSG[20..22], attr_reader.typ);
+        assert_eq!(&MSG[22..24], attr_reader.len);
+        assert_eq!(&MSG[24..28], attr_reader.val);
+    }
+
+    #[test]
+    fn write_raw() {
+        let mut buf = [0u8; 28];
+
+        let writer = RawWriter::new(&mut buf);
+        writer.typ.unwrap();
+    }
 }
